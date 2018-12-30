cadef57dcbf16dc59629e03556498a2c559d41d2
Karsten Senz <karsten.senz@sumcumo.com>
2018-12-31 00:34:49 +0100

    early exit if SERIAL_PORT is undefined

--- serial-to-redis.py
+++ serial-to-redis.py
@@ -1,13 +1,21 @@
 import os
+import sys
 import serial
 import time
 import json
 import redis
 from datetime import datetime
 
-ser = serial.Serial(os.environ.get('SERIAL_PORT'), 9600, timeout=0)
-r = redis.StrictRedis()
+serial_port = os.environ.get('SERIAL_PORT')
+if not serial_port:
+  print('SERIAL_PORT is not defined')
+  sys.exit(1)
 redis_list = os.environ.get('REDIS_LIST') or 'track-events'
+
+
+ser = serial.Serial(serial_port, 9600, timeout=0)
+r = redis.StrictRedis()
+
 
 
 while (True):
